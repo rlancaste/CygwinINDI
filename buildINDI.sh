733a0f6c4abc9c9d94877cff1cb140f23fca703e
Robert Lancaster <rlancaste@gmail.com>
2018-06-11 20:07:56 -0400

    Adding the new cygwin build indi script and the cygwin install bat file

---
+++ buildINDI.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+function statusBanner
+{
+    echo ""
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~ $*"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo ""
+}
+
+statusBanner "This script will setup INDI on cygwin after cygwin and the dependencies are installed using the install.bat script"
+
+read -p "Are you ready to proceed (y/n)? " proceed
+
+if [ "$proceed" != "y" ]
+then
+	exit
+fi
+
+export LIBINDI_REPO=https://github.com/indilib/indi.git
+export INDI_DIR=~/indi-stuff
+
+mkdir -p ${INDI_DIR}
+cd ${INDI_DIR}/
+
+read -p "Do you need to install libnova (y/n)? " installNOVA
+
+if [ "$installNOVA" == "y" ]
+then
+	statusBanner "Installing LibNova"
+	wget -O libnova-0.15.0.tar.gz https://sourceforge.net/projects/libnova/files/libnova-0.15.0.tar.gz
+	tar -xvzf libnova-0.15.0.tar.gz
+	cd libnova-0.15.0
+	autoreconf --install --symlink
+	./configure --prefix=/usr/
+	make
+	make install
+	statusBanner "LibNova is installed."
+	cd ..
+fi
+
+
+if [ ! -d indi ]
+then
+	statusBanner "Cloning indi library"
+	git clone ${LIBINDI_REPO}
+	cd indi/libindi
+else
+	statusBanner "Updating indi"
+	cd indi
+	git pull
+	cd ..
+fi
+
+mkdir -p ${INDI_DIR}/build/libindi
+cd ${INDI_DIR}/build/libindi
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ${INDI_DIR}/indi/libindi
+make
+make install
+
+statusBanner "INDI is installed." 
+
+read -p "Do you want to install GSC (200 MB) to have stars in your INDI CCD Simulator (y/n)? " installGSC
+
+if [ "$installGSC" == "y" ]
+then
+
+	statusBanner "Downloading GSC" 
+	mkdir ~/gsc
+	cd ~/gsc
+	wget -O bincats_GSC_1.2.tar.gz http://cdsarc.u-strasbg.fr/viz-bin/nph-Cat/tar.gz?bincats/GSC_1.2
+	tar -xvzf bincats_GSC_1.2.tar.gz
+	cd src
+	make
+	statusBanner "Please excuse the error above, it will not affect the installation."
+
+	mv a.exe gsc.exe
+	cp gsc.exe /usr/bin/
+
+	cd ..
+	cp ~/gsc/bin/regions.* ~/gsc
+
+	cp ~/.bashrc ~/.bashrc.copy
+	echo "export GSCDAT=~/gsc" >> ~/.bashrc
+
+	statusBanner "GSC is installed"
+fi
---
+++ install.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+REM -- Automates cygwin installation and automatically gets the system set up for INDI Installation
+
+SETLOCAL
+
+REM -- Change to the directory of the executing batch file
+CD %~dp0
+
+REM -- Configure our paths
+SET SITE=http://mirrors.kernel.org/sourceware/cygwin/
+SET LOCALDIR=%LOCALAPPDATA%/cygwin64
+SET ROOTDIR=C:/cygwin64
+
+REM -- These are the packages we will install (in addition to the default packages)
+SET PACKAGES=mintty,wget,git,git-completion,git-svn,p7zip,gcc-core,cmake,make,automake,autoconf,ninja
+SET PACKAGES=%PACKAGES%,zlib-devel,gettext,python,python3,bison,shared-mime-info
+SET PACKAGES=%PACKAGES%,libftdi,eigen,gsl,boost,libboost-devel,libcairo2,libcairo-devel,cfitsio,libcfitsio-devel
+SET PACKAGES=%PACKAGES%,libjpeg-devel,libjpeg-turbo,dcraw,libraw-devel,libtheora-devel,libtiff,libncursesw-devel,libiconv,libfftw3
+
+REM -- Do it!
+ECHO *** INSTALLING PACKAGES
+setup-x86_64.exe --no-admin -q -D -L -d -g -o -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -C Base -P %PACKAGES%
+
+REM -- Show what we did
+ECHO.
+ECHO.
+ECHO cygwin installation updated
+ECHO  - %PACKAGES%
+ECHO.
+
+ENDLOCAL
+
+PAUSE
+EXIT /B 0
